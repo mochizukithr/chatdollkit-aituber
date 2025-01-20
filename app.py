758d900c869b73613bb59d7665598b3819a5ec14
mochizukithr <mochizukithr@gmail.com>
2025-01-20 23:08:26 +0900

    dockerコンテナを作成

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from chatdollkit_aituber import ChatdollKitClient, get_router as get_client_router
+from chatdollkit_aituber.comment import CommentMonitorManager
+from chatdollkit_aituber.comment_api import get_router as get_comment_router
+
+host = os.getenv("CHATDOLLKITAITUBER_HOST", "localhost")
+port = int(os.getenv("CHATDOLLKITAITUBER_PORT", 8888))
+
+print(host,port)
+
+client = ChatdollKitClient(host=host, port=port)
+
+def process_comment(pytchat_comment):
+    client.process_dialog(f"@{pytchat_comment.author.name}:{pytchat_comment.message}")
+
+comment_monitor_manager = CommentMonitorManager(process_comment)
+
+@asynccontextmanager
+async def lifespan(fastapi_app: FastAPI):
+    yield
+    comment_monitor_manager.stop()
+
+
+app = FastAPI(lifespan=lifespan, title="ChatdollKit AITuber Control API")
+app.include_router(get_client_router(client))
+app.include_router(get_comment_router(comment_monitor_manager))
